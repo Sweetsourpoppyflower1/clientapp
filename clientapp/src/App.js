dfeafad930eefe2d1545d238bad5b2d39a7b9508
heise <heisenberg.lex@gmail.com>
2025-12-04 17:16:55 +0100

    i

--- clientapp/src/App.js
+++ clientapp/src/App.js
@@ -1,36 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Register from "./pages/register";
-import Login from "./pages/login";
-import Homepage from "./pages/client/homepage";
-import ActiveAuctions from "./pages/client/ActiveAuctions";
-import UpcomingAuctions from "./pages/client/UpcomingAuctions"
-import Navbar from "./components/Navbar";
-import Clock from "./pages/Clock";
-import AuctionMasterDashboard from "./pages/master/auctionmasterDashboard";
-import AuctionMasterActiveAuctions from "./pages/master/activeAuctionsMA";
-import AuctionMasterUpcomingAuctions from "./pages/master/upcomingAuctionsMA";
-import AuctionMasterCreateAuctions from "./pages/master/createAuctionsMA";
-import SupplierDashboard from "./pages/supplier/supplierDashboard";
-
 
 function App() {
     return (
         <Router>
-            <Navbar />
             <Routes>
-                <Route path="/" element={<Navigate to="/login" replace />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/homepage" element={<Homepage />} />
-                <Route path="/ActiveAuctions" element={<ActiveAuctions />} />
-                <Route path="/UpcomingAuctions" element={<UpcomingAuctions />} />
-                <Route path="/clock" element={<Clock />} />
-                <Route path="/auctionmasterDashboard" element={<AuctionMasterDashboard />} />
-                <Route path="/activeAuctionsMA" element={<AuctionMasterActiveAuctions />} />
-                <Route path="/upcomingAuctionsMA" element={<AuctionMasterUpcomingAuctions />} />
-                <Route path="/createAuctionMA" element={<AuctionMasterCreateAuctions />} />
-                <Route path="/supplierDashboard" element={<SupplierDashboard />} />
+                
             </Routes>
         </Router>
     );
