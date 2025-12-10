31be4b5ff418e2dba40417d05387a0d87e9e3183
Sweetsourpoppyflower1 <abdul2604a@gmail.com>
2025-12-10 14:21:56 +0100

    added dropdown menu, auction screen

---
+++ clientapp/src/pages/master/a_overviewAcceptances.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../../styles/masterPages/a_overviewAcceptances.css";
+
+export default function AOverviewAcceptances() {
+    return (
+        <div className="overview-acceptances-container">
+            <h1>Overview Acceptances</h1>
+            <p>Content coming soon...</p>
+        </div>
+    );
+}
